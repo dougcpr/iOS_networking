8fff61ce7201315e2c32e4552a2999ee047db3df
Doug Cooper <doug.cpr@gmail.com>
2017-08-12 03:16:26 -0500

    updated async dispatch queue

--- iOS Networking/iOS Networking/ViewController.swift
+++ iOS Networking/iOS Networking/ViewController.swift
@@ -13,19 +13,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var moviePoster: UIImageView!
     
     /*  1. Calling the super delegete to intialize view did load function(s): getMoviePoster
-        2. FUNCTION: getMoviePoster()
-         2a. Sets up Session Configuration and URL for HTTP request
-          2b. Intializes local variable, task, to a session data task
-          2c. If no error returns, an HTTP request is made and variable json is
-              cast as a Dictionary<String, any>. The movie db returns 4 parameters:
-              total pages accessible, results (how many hits you received), total potential hits,
-              and the number of pages you hit
-          2d. Assigns a variable to the results array
-          2e. Loops over results array and queries for movie titles, and prints title
-          2f. Start the task.
-    */
+     2. FUNCTION: getMoviePoster()
+     2a. Sets up Session Configuration and URL for HTTP request
+     2b. Intializes local variable, task, to a session data task
+     2c. If no error returns, an HTTP request is made and variable json is
+     cast as a Dictionary<String, any>. The movie db returns 4 parameters:
+     total pages accessible, results (how many hits you received), total potential hits,
+     and the number of pages you hit
+     2d. Assigns a variable to the results array
+     2e. Loops over results array and queries for movie titles, and prints title
+     2f. Start the task.
+     */
     
-
+    
     //************************************//
     //************************************//
     // 1.
@@ -33,7 +33,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -52,15 +52,14 @@ class ViewController: UIViewController {
         let config = URLSessionConfiguration.default
         let session = URLSession(configuration: config)
         let url = URL(string: "https://api.themoviedb.org/3/find/tt0112697?api_key=92cb81ddfc3428a603c8de20d6ee594b&language=en-US&external_source=imdb_id")!
-    
+        
         // 2b. Task Assignment
         let task = session.dataTask(with: url, completionHandler: {(data, response, error) in
             print("task started")
-    
             if error != nil {
-    
+                
                 print(error!.localizedDescription)
-    
+                
             } else {
                 
                 // 2c. Make the HTTP GET Request
@@ -77,7 +76,9 @@ class ViewController: UIViewController {
                             let image = "https://image.tmdb.org/t/p/w500" + poster
                             if let url = URL(string: image) {
                                 if let data = NSData(contentsOf: url as URL) {
-                                    self.moviePoster.image = UIImage(data: data as Data)
+                                    DispatchQueue.main.async {
+                                        self.moviePoster.image = UIImage(data: data as Data)
+                                    }
                                 }
                             }
                             print(poster)
@@ -86,7 +87,7 @@ class ViewController: UIViewController {
                     }
                 } catch {
                     print("error in JSONSerialization")
-    
+                    
                 }
             }
         })
